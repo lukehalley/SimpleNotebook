43d003c5d806b625ff8b3db6b668167dcfaf717e
Luke Halley <luke123halley@gmail.com>
2017-02-27 18:43:31 +1000

    Add comment for list fragment setup

--- app/src/main/java/com/lhkstudios/notebook/MainActivityListFragment.java
+++ app/src/main/java/com/lhkstudios/notebook/MainActivityListFragment.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+// Setup note list fragment with adapter and callbacks
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
