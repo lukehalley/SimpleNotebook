e14b59966981cf7f3dd895cf917b797193fc388b
Luke Halley <luke123halley@gmail.com>
2017-09-04 18:43:29 +1000

    Add comment explaining onCreate initialization

--- app/src/main/java/com/lhkstudios/notebook/MainActivity.java
+++ app/src/main/java/com/lhkstudios/notebook/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     public enum FragmentToLaunch{ VIEW, EDIT}
 // Initialize main activity with note list view
 
+// Initialize UI components and set up event listeners
 // Initialize fragment manager and load the note list view
     @Override
     protected void onCreate(Bundle savedInstanceState) {
